ee4a6f8bbeb70a83a8a25a09f175fe392137b067
developer-demon <beyzabulut.anon@gmail.com>
2024-06-09 20:03:52 +0200

    siparis kayit gönderildi

---
+++ kargotakip.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import sqlite3
+
+ekran=Tk()
+ekran.title("Kargo takip")
+ekran.geometry("900x500+800+200")
+
+
+kargoTakip=Entry()
+kargoTakip.pack()
+
+kargoTakipnumarasi=Entry()
+kargoTakipnumarasi.pack()
+
+KargoyaverilmeTarihi=Entry()
+
+
+
+ekran=mainloop()
